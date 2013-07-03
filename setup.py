283e9ba2f2c9417f6284768e780d65a8006a685c
Jakub Stasiak <jakub@stasiak.at>
2013-07-03 11:59:42 +0200

    Fix version reading in setup

--- setup.py
+++ setup.py
@@ -2,28 +2,31 @@ from os.path import abspath, dirname, join
 
 from setuptools import setup
 
-from pykka_injector import __version__
-
 PROJECT_ROOT = abspath(dirname(__file__))
 
 long_description = open(join(PROJECT_ROOT, 'README.rst')).read()
 description = 'Pykka/Injector integration module'
 
-setup(
-    name='pykka_injector',
-    url='http://github.com/jstasiak/pykka_injector',
-    download_url='http://pypi.python.org/pypi/pykka_injector',
-    version=__version__,
-    description=description,
-    long_description=long_description,
-    license='MIT',
-    platforms=['any'],
-    py_modules=['pykka_injector'],
-    author='Jakub Stasiak',
-    author_email='jakub@stasiak.at',
-    install_requires=[
-        'setuptools >= 0.6b1',
-        'pykka',
-        'injector',
-    ],
-)
+module_code = open(join(PROJECT_ROOT, 'pykka_injector.py')).readlines()
+line = [line for line in module_code if line.startswith('__version__ = ')][0]
+version = line.split('=')[-1].strip().strip("'")
+
+if __name__ == '__main__':
+    setup(
+        name='pykka_injector',
+        url='http://github.com/jstasiak/pykka_injector',
+        download_url='http://pypi.python.org/pypi/pykka_injector',
+        version=version,
+        description=description,
+        long_description=long_description,
+        license='MIT',
+        platforms=['any'],
+        py_modules=['pykka_injector'],
+        author='Jakub Stasiak',
+        author_email='jakub@stasiak.at',
+        install_requires=[
+            'setuptools >= 0.6b1',
+            'pykka',
+            'injector',
+        ],
+    )
